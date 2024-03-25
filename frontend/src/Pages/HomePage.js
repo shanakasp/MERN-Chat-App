336b1f65e0987c0ae38f9bc83ad7cd849fb86037
shanakasp <74164029+shanakasp@users.noreply.github.com>
2024-03-25 13:38:17 +0530

    changed 5 files

--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -1,10 +1,62 @@
+import {
+  Box,
+  Container,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Text,
+} from "@chakra-ui/react";
 import React from "react";
+import Login from "../Components/Auth/Login";
+import SignUp from "../Components/Auth/SignUp";
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <Container maxW="xl" centerContent>
+      <Box
+        d="flex"
+        justifyContent="center"
+        p={3}
+        bg="beige"
+        w="100%"
+        m={"40px 0 15px 0"}
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Text
+          fontSize="4xl"
+          fontFamily="Work Sans"
+          color="black"
+          textAlign="center"
+        >
+          Talk - A - Tive
+        </Text>
+      </Box>
+      <Box
+        bg={"#E6E5C2"}
+        w={"100%"}
+        p={4}
+        borderRadius={"lg"}
+        borderWidth={"1px"}
+      >
+        <Tabs variant="soft-rounded">
+          <TabList>
+            <Tab width={"50%"}>Login</Tab>
+            <Tab width={"50%"}>Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login></Login>
+            </TabPanel>
+            <TabPanel>
+              <SignUp></SignUp>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
   );
 };
 
